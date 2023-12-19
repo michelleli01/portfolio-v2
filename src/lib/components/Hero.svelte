a3fbbc59c6ec6454721a6773d686e1edbaaeb110
Michelle Li <michelleyli123@gmail.com>
2023-12-19 18:31:09 -0500

    hero page

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,20 @@
+<section
+	class="font-serif text-general flex justify-between items-end content-between flex-shrink-0 flex-wrap"
+>
+	<div>
+		<h1 class="text-title self-stretch font-semibold">
+			Hey, my name is <div class="text-primary">Michelle</div>
+		</h1>
+		<p class="text-heading-3 self-stretch font-medium">software engineer @ microsoft</p>
+	</div>
+
+	<div
+		class="font-medium text-heading-3 tracking-widest flex justify-between items-center flex-shrink-0"
+	>
+		<div>
+			<p>web developer</p>
+			<p>forever student</p>
+		</div>
+		<p>based in new york city</p>
+	</div>
+</section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
-  import "../app.css";
+	import '../app.css';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
-<slot />
+<section class="h-screen bg-gray px-4 md:px-8 lg:px-40 pt-12">
+	<Header />
+	<slot />
+</section>
