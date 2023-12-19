4e72d8e9c0e95dcb9b915b48eafff6b1080727d5
Michelle Li <michelleyli123@gmail.com>
2023-12-19 18:31:02 -0500

    tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,30 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		screens: {
-			sm: '480px',
-			md: '768px',
-			lg: '976px',
-			xl: '1440px'
-		},
-		colors: {
-			'text-gray': '#1E1A15',
-			'background-gray': '#F9F8F6',
-			'primary-green': '#305F27',
-			'secondary-beige': '#E6E2D4',
-			'accent-brown': '#6E5A3B'
-		},
-		fontFamily: {
-			serif: ['EB Garamound', 'serif'],
-			sans: ['Inter', 'sans-serif']
-		},
-		extend: {}
+		extend: {
+			letterSpacing: {
+				widest: '.15em'
+			},
+			fontSize: {
+				title: ['clamp(3.125rem, 5vw, 7.5rem)'],
+				'heading-1': ['clamp(1.85rem, 1vw, 3.75rem)'],
+				'heading-2': ['clamp(2.4rem, 8vw, 10rem)'],
+				'heading-3': ['clamp(1.25rem, 1vw, 1.875rem)'],
+				body: '1rem'
+			},
+			colors: {
+				transparent: 'transparent',
+				general: '#1E1A15',
+				gray: '#F9F8F6',
+				primary: '#305F27',
+				secondary: '#B2A38C',
+				accent: '#625035'
+			},
+			fontFamily: {
+				serif: ['EB Garamound', 'serif'],
+				sans: ['Inter', 'sans-serif']
+			}
+		}
 	},
 	plugins: []
 };
