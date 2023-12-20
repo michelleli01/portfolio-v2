f2837daf8bad3dcc666996e8c91810ea7b78869f
Michelle Li <michelleyli123@gmail.com>
2023-12-19 19:56:28 -0500

    mobile responsive

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,33 +3,31 @@
 </script>
 
 <section class="font-serif text-general flex-wrap mt-10">
-	<div class="w-full mb-40">
+	<div class="w-full mb-20 md:mb-36 lg:mb-40">
 		<h1 class="text-title font-semibold">
 			Hey, my name is <div class="text-primary">Michelle</div>
 		</h1>
 		<p class="text-heading-3 font-medium">software engineer @ microsoft</p>
 	</div>
 
-	<div>
-		<div class="font-medium text-heading-3 tracking-widest flex justify-between items-center">
-			<div class="flex flex-col items-start text-center">
-				<div class="flex gap-2">
-					<p>web</p>
-					<p class="text-primary">developer</p>
-				</div>
-				<div class="flex gap-2">
-					<p>forever</p>
-					<p class="text-primary">student</p>
-				</div>
+	<div class="font-medium text-heading-3 tracking-widest flex justify-between items-center">
+		<div class="flex flex-col items-start text-center hidden md:flex">
+			<div class="flex gap-2">
+				<p>web</p>
+				<p class="text-primary">developer</p>
 			</div>
 			<div class="flex gap-2">
-				<p>based in</p>
-				<p class="text-primary">new york city</p>
+				<p>forever</p>
+				<p class="text-primary">student</p>
 			</div>
+		</div>
+		<div class="flex gap-2 mx-auto md:mx-0">
+			<p>based in</p>
+			<p class="text-primary">new york city</p>
 		</div>
 	</div>
 
-	<div class="flex justify-center items-center">
+	<!-- <div class="flex justify-center items-center">
 		<img src={scroll_circle} alt="circle" />
-	</div>
+	</div> -->
 </section>
