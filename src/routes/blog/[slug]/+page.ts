fa7c1540223a807f606fd92b58133f4f5c31546b
Michelle Li <michelleyli123@gmail.com>
2023-12-21 16:44:55 -0500

    syntax highlighting

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -3,7 +3,7 @@ import { error } from '@sveltejs/kit';
 export async function load({ params }) {
 	try {
 		const post = await import(`../../../posts/${params.slug}.md`);
-		console.log(post);
+
 		return {
 			content: post.default,
 			meta: post.metadata
