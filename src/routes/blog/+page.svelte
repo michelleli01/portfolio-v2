d68f7ce03e786dc82177580a809d4d0bd8d85d7b
Michelle Li <michelleyli123@gmail.com>
2023-12-21 15:51:52 -0500

    work in progress

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,6 @@
-<section class="h-screen"></section>
+<section
+	class="h-screen font-sans text-general text-heading-3 m-20 flex flex-col items-center justify-center"
+>
+	<p class="">This page is currently a work in progress 🚧</p>
+	<p>Come back soon!</p>
+</section>
