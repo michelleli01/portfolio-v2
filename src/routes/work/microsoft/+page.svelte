1d6402cddf6b8fc13c75ad04a8c2699cfe77fdf0
Michelle Li <michelleyli123@gmail.com>
2023-12-22 21:19:22 -0500

    microsoft exp

--- src/routes/work/microsoft/+page.svelte
+++ src/routes/work/microsoft/+page.svelte
@@ -4,9 +4,98 @@
 <svelte:head>
 	<title>microsoft | michelle li</title>
 </svelte:head>
-<section
-	class="h-screen font-sans text-general text-heading-3 flex flex-col items-center justify-center"
->
-	<p class="">This page is currently a work in progress 🚧</p>
-	<p>Come back soon!</p>
+
+<section class="py-10 divide-y-4 divide-secondary divide-opacity-60">
+	<div class="flex flex-col md:flex-row justify-between items-center gap-12 pb-8 md:pb-20">
+		<div>
+			<div class="flex flex-col justify-center items-center">
+				<h1 class="font-serif text-primary font-semibold text-heading-1">
+					Software Engineer Intern @ Microsoft
+				</h1>
+				<div>
+					<h3 class="font-sans text-accent font-medium text-heading-3">
+						Azure VmWare Solutions Infrastructure Services Team
+					</h3>
+					<p class="font-sans text-gray-2 font-medium text-body">Summer 2022 & Summer 2023</p>
+				</div>
+			</div>
+
+			<div class="font-sans text-general text-body pt-6 flex flex-col gap-4">
+				<div>
+					<b class="font-semibold text-heading-3">Overview</b>
+					<p>
+						I worked as a Software Engineer Intern at Microsoft for two summers: after my sophomore
+						year and after my junior year. Throughout my time on the team, I was honored to have the
+						opportunity to learn from and work with a talented group of engineers. Due to the
+						sensitive nature of the projects, I am unable to publicly share my work on the
+						Infrastructure Services team. If you're interested in hearing more about my work at
+						Microsoft, please <a
+							href="mailto:myl39@cornell.edu"
+							class="text-gray-2 font-bold underline">email me</a
+						>!
+					</p>
+				</div>
+				<div class="flex flex-col gap-4">
+					<div>
+						<h3 class="font-semibold text-heading-3">Summer 2022</h3>
+						I started at Microsoft during summer 2022 after my sophomore year and my primary responsbilities
+						were to:
+						<ul class="list-decimal list-inside">
+							<li>Onboard onto the Infrastructure Services team</li>
+							<li>
+								Develop an automation script that validates a list of criteria for resource
+								deployment to Azure VmWare Solution (AVS) servers
+							</li>
+						</ul>
+					</div>
+
+					<div class="flex flex-col gap-4">
+						<div>
+							<h3 class="font-semibold text-heading-3">Summer 2023</h3>
+							I returned again to Microsoft during summer 2023, after my junior year, and as a returning
+							intern to the same team, the complexity and scope of the my project increased.
+							<!-- <ul class="list-decimal list-inside">
+								<li>Onboard onto the Infrastructures team</li>
+								<li>
+									Develop an automation script that validates a list of criteria for resource
+									deployment to Azure VmWare Solution (AVS) servers
+								</li>
+							</ul> -->
+						</div>
+
+						<div class="flex flex-col gap-2">
+							<h3 class="font-semibold text-heading-3">Learnings</h3>
+
+							<div>
+								Here are some of takeways I gained from this this entire experience:
+								<ul class="list-decimal list-outside pl-5">
+									<li>
+										<p class="font-semibold">
+											Be proactive and take ownership of your own learning.
+										</p>
+										It's quite intimidating at first to reach out to senior-level engineers, but that
+										is the best way to properly learn the system.<br /><i
+											>Go to the person who wrote it and ask lots of questions!</i
+										>
+									</li>
+									<li>
+										It's ok to make mistakes! You're not going to get everything exactly right the
+										first time and that is totally ok. <i
+											>Stay open to feedback and continue to grow from them.</i
+										>
+									</li>
+									<li>
+										Don't worry too much about the details. With such an extensive codebase
+										containing hundreds of thousands of lines of code, it is impossible to fully
+										understand every line and every microservice. Focus on what you need to know and
+										gain a high level understanding. Everything else will come to you as you work.
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
